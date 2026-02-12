da37ab9e5bd06a482d40227f411e9202a0563ae5
R4dowan <97108939@st.deltion.nl>
2026-02-12 11:05:00 +0100

    alles

--- app/020_tekstfuncties/opdr_1/opdr_1.py
+++ app/020_tekstfuncties/opdr_1/opdr_1.py
@@ -2,18 +2,16 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code...
-
 # Opdracht 1a
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
 
-print()
+print("O mamma mia, wat maak je toch heerlijke pizza's")
 
 # Gebruik onderstaande variabelen en de print-functie
-# en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
+# en toon de volgende zin op het scherm:
+# Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
 voornaam = "albert"
 achternaam = "einstein"
 
-
-print()
+print(voornaam.capitalize(), achternaam.capitalize(), "zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'")
--- app/020_tekstfuncties/opdr_3/opdr_3.py
+++ app/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,22 @@
-# Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
+# Opdracht 3
+# Naam student: Radwan Jabri
+# Groep: ...
 
-# Hier komt je code...
+# Eén boom bestaat uit deze 8 regels:
+boom = [
+    "    *    ",
+    "   ***   ",
+    "  *****  ",
+    " ******* ",
+    "*********",
+    "  *****  ",
+    "   ***   ",
+    "   ***   "
+]
+
+# Aantal bomen naast elkaar
+aantal_bomen = 5
+
+# Print elke rij van de bomen naast elkaar
+for regel in boom:
+    print(regel * aantal_bomen)
